a1ff3f29cbe98a64163370ff816caf7bfb963b95
elias1991xNataly <131762074+elias1991xNataly@users.noreply.github.com>
2025-01-24 09:16:04 +0100

    changing eslint.config.mjs to eslintrc.json

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+
+type Props = {
+  title: string
+};
+
+
+export default function Home({ title }: Props) {
+  return (
+    <div>
+      {" "}
+      <h1>{title}</h1>
+      <p>TypeScript is now enabled!</p>
+      {" "}
+    </div>
+  );
+}
